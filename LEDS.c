27e11aa0dfcaf3175eb36dd1669cb576afeb9858
Hassan Elsaied <40128266+hassan22EL@users.noreply.github.com>
2018-06-23 05:37:33 +0200

    Update LEDS.c

--- LEDS.c
+++ LEDS.c
@@ -9,19 +9,24 @@
 #include <xc.h>
 #include "LEDS.h"
 #define _XTAL_FREQ  16000000
+#include "../inulde/ADC.X/ADC.h"
 
 void INIT_LEDS() {
+    ADC_Init(1, 0); //select channel AN1 and select Clock fosc/2 
+    ANDEL = 0x00; // All pins Analog 
     TRISDbits.TRISD0 = 0; // pin output on  LED BL_LED
     TRISDbits.TRISD1 = 0; //  pin output on  LED BR_LED 
     TRISDbits.TRISD4 = 0; // pin output on  LED FR_LED
     TRISDbits.TRISD5 = 0; //pin output on  LED FL_LED
+    TRISAbits.TRISA1 = 1; //this pins input 
     BL_LED = 0; //oFf BL_LED 
     BR_LED = 0; //oFf BR_LED
     FR_LED = 0; //oFf FR_LED
     FL_LED = 0; //oFf FL_LED
 }
-void LED_OFF(){
-   BL_LED = 0; //oFf BL_LED 
+
+void LED_OFF() {
+    BL_LED = 0; //oFf BL_LED 
     BR_LED = 0; //oFf BR_LED
     FR_LED = 0; //oFf FR_LED
     FL_LED = 0; //oFf FL_LED
@@ -57,11 +62,30 @@ void Car_Right() {
 }
 
 void Car_Back() {
-   BR_LED  = 1;
+    BR_LED = 1;
     BL_LED = 1;
 }
 
 void Car_Forward() {
-    FR_LED=1;
-    FL_LED=1;
+    int a = Read_ADC(1);
+    if (a == 0) {
+
+        FR_LED = 1;
+        FL_LED = 1;
+    } else {
+        FR_LED = 0;
+        FL_LED = 0;
+    }
+}
+
+void Car_BackS() {
+    int a = Read_ADC(1);
+    if (a == 0) {
+        BR_LED = 1;
+        BL_LED = 1;
+    } else {
+        BR_LED = 0;
+        BL_LED = 0;
+    }
+
 }
