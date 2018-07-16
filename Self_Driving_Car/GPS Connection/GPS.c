59c69f13d3f02f034abb08958fc2a403ecd58d02
Hassan Elsaied <40128266+hassan22EL@users.noreply.github.com>
2018-07-16 20:57:42 +0200

    Add files via upload

--- Self_Driving_Car/GPS Connection/GPS.c
+++ Self_Driving_Car/GPS Connection/GPS.c
@@ -2,140 +2,167 @@
  * GPS.c
  *
  * Created: 7/10/2018 4:17:22 PM
- *  Author: hassa
+ *  Author:Hassan Elsaied 
+                                         *****The National Marine Electronics Association Protocol (NMEA)) 
+										 /*
+										 NMEA Working as asynchronous serial interface with the following parameters:
+										 Baud rate: 9
+										 Number of data bits: 8 (bit 7 is 0)
+										 Stop bits: 1 (or more)
+										 Parity: none
+										 Handshake: none
+										 NMEA 0183 allows a single talker and several listeners on one circuit
+										 Data sender From NMEA protocol in
+										 GGA Global Positioning System Fix Data. Time, Position and fix related data for a GPS receiver
+										 
+										$--GGA,hhmmss.ss,llll.ll,a,yyyyy.yy,a,x,xx,x.x,x.x,M,x.x,M,x.x,xxxx*hh
+										1) Time (UTC)
+										2) Latitude
+										3) N or S (North or South)
+										4) Longitude
+										5) E or W (East or West)
+										6) GPS Quality Indicator,
+										0 - fix not available,
+										1 - GPS fix,
+										2 - Differential GPS fix
+										7) Number of satellites in view, 00 - 12
+										8) Horizontal Dilution of precision
+										9) Antenna Altitude above/below mean-sea-level (geoid)
+										10) Units of antenna altitude, meters
+										11) Geoidal separation, the difference between the WGS-84 earth
+										ellipsoid and mean-sea-level (geoid), "-" means mean-sea-level below ellipsoid
+										12) Units of geoidal separation, meters
+										13) Age of differential GPS data, time in seconds since last SC104
+										type 1 or 9 update, null field when DGPS is not used
+										14) Differential reference station ID, 0000-1023
+										15) Checksum
  */ 
 #include <avr/io.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-#include <stdint.h>
 #include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 #include "/projects/AVR/TRacking Car/TRacking Car/GPS.h"
 #include "/projects/AVR/EUSART/EUSART/EUSART.h"
 #define F_CPU 16000000UL
-#define NMEA_BUFFER_LEN 85 //Maximum length of a NEMA string for buffer allocation
-volatile char *buffer;  //Pointer to our current RX buffer
-volatile uint8_t buffer_index = 0;//Counter for our current position in the buffer.
-//We don't check that (buffer_index < bufferSize) because we know the NEMA limits
-nmeaData *gpsData; //Pointer to our linked list of NEMA strings
-int parseGPRMC(char *item){
-	char out[NMEA_BUFFER_LEN]="";
-	fixData Fix ;
-	memset(&Fix,0x00,sizeof(Fix));//Date ,Time , latitude ,longitude equles zeros
-	/* split string */
-	uint8_t pos =0 ;
-	uint8_t DecimalPos =0;
-	char *token =strtok(item,","); 
-	while(token !=NULL){
-		pos++;
-		switch(pos){
-			case 1:
-			break ;
-			case 2:
-			Fix.time =atol(token);//converted String to long int 
-			break;
-			case 3 :
-			if(token[0]!=0x41)return 1 ;
-			break ;
-			case 4: //Copy latitude to fixData element
-			for(DecimalPos=0;DecimalPos<strlen(token) ; DecimalPos++){
-				if(token[DecimalPos]==0x2E) //look for "." 
-				break;
-			}
-			if(DecimalPos ==strlen(token)) return 1 ;
-			//Read First half
-			token[DecimalPos]=0x00 ; //set value in point off 
-			Fix.lat1=atol(token); //converted String to long int 
-			Fix.lat2=atol(&token[DecimalPos+1]); //
-			break;
-			case  5://Add North / South to fixData element (N/S)
-			memcpy(&Fix.latDir,token,sizeof(char));//Copy token in latDir 
-			break;
-			case 6://Copy longitude to fixData element
-			for(DecimalPos=0;DecimalPos <strlen(token);DecimalPos++){
-				if(token[DecimalPos]==0x2E); //look for "." 
-				break ;
-			}
-			if(token[DecimalPos]==strlen(token)) return 1 ;
-			//Read First half ;
-			token[DecimalPos]==0x00 ;
-			Fix.lon1=atol(token);
-			Fix.lon2=atol(&token[DecimalPos+1]);
-			break;
-			case 7 :
-			//Add East /west to Fix Elemnt 
-			memcpy(&Fix.lonDir,token,sizeof(char));
-			break ;
-			case  8 :
-			break ;
-			case 9 :
-			break ;
-			case 10:
-			//data 
-			Fix.date =atol(token);
-			break ;
-			case 11:
-			break ;
-			case 12 :
-			break ;
-			case 13 :
-			break ;
-			default:
-			break ;
-	} 
+void convert_time_to_UTC(){
+	unsigned int Hour,Minuet,Second ;
+	uint32_t Time_Value ;
+	Time_Value=atol(Time_Buffer); //  convert string to long integer value 
+	// for Example  the Time Value= 145436  (14:54:36)  Hour =14 , minuet =54 ,sec=36 
+	Hour =(Time_Value/10000); // extract hour from integer  145436/10000 =14 remained =5436
+	//if Remained /100 =54 
+	Minuet=(Time_Value%10000)/100 ; // extract minuet from integer 5436/100=54 and Remained2=36 
+	Second=(Time_Value%10000)%100; // extract second from integer 
+	sprintf(Time_Buffer,"%d:%d:%d",Hour,Minuet,Second);
+	
+}
+char *convert_to_degrees(char *Row){
+	double value;
+	float Decimal_Vaule ,temp ;
+	int32_t  Degrees;
+	float Position;
+	value=atof(Row); //Convert String (latitude or longitude ) to float Number ;
+	/* convert Raw latitude/longitude into degree format */
+	Decimal_Vaule =(value/100 );
+	Degrees =(int)Decimal_Vaule ;
+	temp=(Decimal_Vaule-(int )Decimal_Vaule)/.6 ;
+	Position =(float)Degrees+temp;
+ char *String=dtostrf(Position,6,4,degrees_buffer); //convert float value into string 
+ return String ;
 
 	}
-	
-	return 0;
-}
+void GPS_Time(){
+	cli(); //clear Global interrupt 
+	uint8_t time_index =0 ;
+	/* parse Time in GGA string stored in buffer */
+	for(uint8_t index=0; GGA_Buffer[index]!=',' ;index++){
+		Time_Buffer[time_index]=GGA_Buffer[index];
+		time_index++ ;
+		}
+		convert_time_to_UTC();
+		sei(); //Set Global Interrupt Flag
+	}
+char *GPS_latitude(uint16_t lat_pointer){
+	cli(); //clear Global interrupt 
+	uint8_t lat_index ;
+	uint8_t index=lat_pointer+1;
+	lat_index=0;
+	/* parse Latitude in GGA string stored in buffer */
+	for(;GGA_Buffer[index]!=',';index++){
+	Latitude_Buffer[lat_index]=GGA_Buffer[index];
+	lat_index++;
+	}
+	/*Get  Directions*/
+	Latitude_Buffer[lat_index++]=GGA_Buffer[index++];
+	Latitude_Buffer[lat_index]=GGA_Buffer[index] ;//Get directions N/S
+	char * latitude =convert_to_degrees(Latitude_Buffer);
+	sei();//Set Global Interrupt Flag
+	return latitude ;
+	}
+char *GPS_longitude(uint16_t long_pointer){
+    cli(); //clear Global interrupt 
+	uint8_t long_index ;
+	uint8_t index =long_pointer+1;
+	long_index=0;
+		/* parse Longitude in GGA string stored in buffer */
+		for (;GGA_Buffer[index]!=',';index++){
+	    Longitude_Buffer[long_index]=GGA_Buffer[index];
+		long_index++;
+		}
+		/*Get  Directions*/
+		Longitude_Buffer[long_index++]=GGA_Buffer[index++];
+		Longitude_Buffer[long_index]=GGA_Buffer[index]; //Get directions E/W
+		char *longitude=convert_to_degrees(Longitude_Buffer);
+		sei();//Set Global Interrupt Flag
+		return longitude ;
 		
-void Entry_New(){
-	if(gpsData->next !=0){
-		cli();// Clear Global Interrupt Flag
-		volatile nmeaData *itme =gpsData ;
-		gpsData=gpsData->next ;
-		sei();//set 
-		if(strncmp(itme->nmeaString,"$GPRMC",6)==0){//compared between String 1 and string 2 
-            parseGPRMC(itme->nmeaString);
-			} 
-		free((void *)itme->nmeaString);
-		free((void *)itme);
 	}
-}
-GPS_INIT(){
-	gpsData=malloc(sizeof(nmeaData));
-	gpsData->next=0;
-	buffer=malloc(sizeof(char)*NMEA_BUFFER_LEN);
-	_delay_ms(80);
-	EUART_Start(9600);
-}
-ISR(EUSART_RX_Vector){//External interrupt From Rx pin
-	uint8_t c;
-	c=UDR;
-	buffer[buffer_index]=c;
-	buffer_index++;
-	if(c=='\n'){
-		volatile nmeaData *item =gpsData ;
-		while (item->next!=0)
-		item=item->next;
-		item=(nmeaData *)malloc(sizeof(nmeaData));
-		if(item->next==NULL){
-			buffer_index=0;
-			return ;
-			
-		}
-		item=item->next;
-		item->nmeaString=buffer;
-		buffer=malloc(sizeof(char)*NMEA_BUFFER_LEN);
-		if(buffer==NULL){
-			buffer=item->nmeaString;
-			buffer_index=0;
-			return;
-		}
-		item->next=0;
-		buffer_index=0;
+void GPS_Altitude(uint16_t Alt_pointer){
+	cli(); //clear Global interrupt 
+	uint8_t alt_index ;
+	uint8_t   index =Alt_pointer+1;
+	alt_index=0;
+	/* parse Altitude in GGA string stored in buffer */
+	for (;GGA_Buffer[index]!=',';index++)
+	{
+		Altitude_Buffer[alt_index]=GGA_Buffer[index];
+		alt_index++;
 	}
+	Altitude_Buffer[alt_index]=GGA_Buffer[index+1];
+	sei();//Set Global Interrupt Flag
 	
-	
+}
+void GPS_INIT(){
+	memset(GGA_Buffer,0,Buffer_Size);
+	memset(degrees_buffer,0,degrees_buffer_size);
+	EUSART_Start(9600);
+}
+ISR (USART_RXC_vect){
+	uint8_t oldsrg=SREG;
+	cli(); //clear Global interrupt 
+	char Recived_char=UDR ;
+	if(Recived_char=='$'){
+		GGA_Index =0;
+		CommaCounter=0;
+		GGA_String=false;
+	}else if(GGA_String==true){
+		   /* if true save GGA info. into buffer */
+		   if(Recived_char==',')
+		   GGA_Pointers[CommaCounter++]=GGA_Index ;    /* store instances of ',' in buffer */
+		   GGA_Buffer[GGA_Index++]=Recived_char ;
+		     }else if(GGA_CODE[0]=='G' && GGA_CODE[1]=='G' && GGA_CODE[2]=='A'){ /* check for GGA string */
+				 GGA_String=true ;
+				 GGA_CODE[0]=0;
+				 GGA_CODE[1]=0;
+				 GGA_CODE[2]=0;
+				 
+			 }else{
+				 GGA_CODE[0]=GGA_CODE[1];
+				 GGA_CODE[1]=GGA_CODE[2];
+				 GGA_CODE[2]=Recived_char;
+			 }
+			 SREG = oldsrg;
 }
--- Self_Driving_Car/GPS Connection/GPS.h
+++ Self_Driving_Car/GPS Connection/GPS.h
@@ -1,61 +1,68 @@
 /*
- * GPS.h
- *
- * Created: 7/10/2018 4:16:59 PM
- *  Author: hassa
- */ 
+* GPS.h
+*
+* Created: 7/10/2018 4:16:59 PM
+*  Author: hassa
+*/
 
 
 #ifndef GPS_H_
 #define GPS_H_
+/*
+GGA Global Positioning System Fix Data. Time, Position and fix related data
+for a GPS receiver
 
-struct _nmeaData {
-	///Pointer to the NEMA string
-	char *nmeaString;
-	///Pointer to the next NEMA object
-	struct _nmeaData * next;
-};
-typedef volatile struct _nmeaData nmeaData;
-///Struct to store fixes as addressible parts rather than a string - 26 bytes
-struct _fixData {
-	
-	char latDir;///N/S direction
-	char lonDir;//E/W direction
-	uint32_t date;//Date as ddmmyy
-	uint32_t time;//Time as hhmmss UTC
-	uint32_t lat1;//First portion of the latitude decimal
-	uint32_t lat2;//Second portion of the latitude decimal
-	uint32_t lon1;	//First portion of the longitude decimal
-	uint32_t lon2;//Second portion of the longitude decimal
-};
-
-typedef struct _fixData fixData;
-
-//Functions
-/*Interpreted sentences
-`$GPBOD - Bearing, origin to destination
-$GPBWC - Bearing and distance to waypoint, great circle
-$GPGGA - Global Positioning System Fix Data
-$GPGLL - Geographic position, latitude / longitude
-$GPGSA - GPS DOP and active satellites
-$GPGSV - GPS Satellites in view
-$GPHDT - Heading, True
-$GPR00 - List of waypoints in currently active route
-$GPRMA - Recommended minimum specific Loran-C data
-$GPRMB - Recommended minimum navigation info
-$GPRMC - Recommended minimum specific GPS/Transit data
-$GPRTE - Routes
-$GPTRF - Transit Fix Data
-$GPSTN - Multiple Data ID
-$GPVBW - Dual Ground / Water Speed
-$GPVTG - Track made good and ground speed
-$GPWPL - Waypoint location
-$GPXTE - Cross-track error, Measured
-$GPZDA - Date & Time*/
-int parseGPRMC(char *item);// Recommended minimum specific GPS/Transit data
-void Entry_New();
-GPS_INIT();
-
-
+1         2       3 4        5 6 7  8   9   10 11 12 13  14 15
+|         |       | |        | | |  |   |   |  |  |  |   |  |
+Data =$--GGA,hhmmss.ss,llll.ll,a,yyyyy.yy,a,x,xx,x.x,x.x,M,x.x,M,x.x,xxxx*hh
+1) Time (UTC)
+2) Latitude
+3) N or S (North or South)
+4) Longitude
+5) E or W (East or West)
+6) GPS Quality Indicator,
+0 - fix not available,
+1 - GPS fix,
+2 - Differential GPS fix
+7) Number of satellites in view, 00 - 12
+8) Horizontal Dilution of precision
+9) Antenna Altitude above/below mean-sea-level (geoid)
+10) Units of antenna altitude, meters
+11) Geoidal separation, the difference between the WGS-84 earth
+ellipsoid and mean-sea-level (geoid), "-" means mean-sea-level below ellipsoid
+12) Units of geoidal separation, meters
+13) Age of differential GPS data, time in seconds since last SC104
+type 1 or 9 update, null field when DGPS is not used
+14) Differential reference station ID, 0000-1023
+15) Checksum
+*/
+#include <avr/io.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#define Buffer_Size 150
+#define degrees_buffer_size 20
+#define SREG    _SFR_IO8(0x3f)
+#define F_CPU 16000000UL
+char    Latitude_Buffer[15];
+char    Longitude_Buffer[15];
+char    Time_Buffer[15];
+char    Altitude_Buffer[8];
+char    degrees_buffer[degrees_buffer_size];   /* save latitude or longitude in degree */
+char    GGA_Buffer[Buffer_Size];               /* save GGA string */
+uint8_t GGA_Pointers[20];                   /* to store instances of ',' */
+char    GGA_CODE[3];
+volatile uint16_t GGA_Index, CommaCounter;
+bool GGA_String ,Flag1, Flag2;
+void convert_time_to_UTC();
+char *convert_to_degrees(char *Row);
+void GPS_Time();/* parse Time in GGA string stored in buffer */
+char *GPS_latitude(uint16_t lat_pointer);/* parse Latitude in GGA string stored in buffer */
+char *GPS_longitude(uint16_t long_pointer);/* parse Longitude in GGA string stored in buffer */
+void GPS_Altitude(uint16_t Alt_pointer);/* parse Altitude in GGA string stored in buffer */
+void GPS_INIT();
 
 #endif /* GPS_H_ */
